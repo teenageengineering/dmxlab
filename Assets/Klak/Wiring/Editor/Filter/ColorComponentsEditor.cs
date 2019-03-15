164a1188d3f45f2dd1036d99fb8e20c8e3931989
finn <finn.ericson@gmail.com>
2019-03-15 17:11:19 +0100

    Add ColorComponents Klak node

---
+++ Assets/Klak/Wiring/Editor/Filter/ColorComponentsEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Wiring
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ColorComponents))]
+    public class ColorComponentsEditor : Editor 
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertiesExcluding(serializedObject, new string[] {"m_Script"});
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Klak/Wiring/Filter/ColorComponents.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Convertion/Color Components")]
+    public class ColorComponents : NodeBase
+    {
+        #region Node I/O
+
+        [Inlet]
+        public Color color {
+            set {
+                if (!enabled) 
+                    return;
+
+                _redEvent.Invoke(value.r);
+                _greenEvent.Invoke(value.g);
+                _blueEvent.Invoke(value.b);
+                _alphaEvent.Invoke(value.a);
+            }
+        }
+
+        [SerializeField, Outlet]
+        FloatEvent _redEvent = new FloatEvent();
+
+        [SerializeField, Outlet]
+        FloatEvent _greenEvent = new FloatEvent();
+
+        [SerializeField, Outlet]
+        FloatEvent _blueEvent = new FloatEvent();
+
+        [SerializeField, Outlet]
+        FloatEvent _alphaEvent = new FloatEvent();
+
+        #endregion
+    }
+}
